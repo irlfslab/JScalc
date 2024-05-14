6472c7f506f48ca31dff25ccefa2f04f04b5f1f5
IL <53288945+irlfslab@users.noreply.github.com>
2024-05-14 18:11:49 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const disp = document.getElementById("display");
+let prev='';
 function addKey(inval){
     disp.value += inval;
 }
 function clearDisp(){
     disp.value = "";
 }
+function deleteNum(){
+    disp.value = disp.value.toString().slice(0,-1);
+}
 function calculate(){
     try{disp.value = eval(disp.value)}
     catch(error){disp.value = "Error"}
+
 }
